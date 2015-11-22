90319cc887e7a29d57d8b60f49da7a7730b84093
bersling <bersling@gmail.com>
2015-11-22 12:31:06 +0100

    update routes and restructure more...

---
+++ client/routes.js
@@ -0,0 +1,22 @@
+
+angular.module('h2c').config(function($urlRouterProvider, $stateProvider, $locationProvider){
+
+  $locationProvider.html5Mode(true);
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'client/components/home/home.html',
+      controller: 'HomeController'
+    }).state('search', {
+      url: '/search',
+      templateUrl: 'client/components/search/search.html',
+      controller: 'SearchController'
+    }).state('submit', {
+      url: '/submit',
+      templateUrl: 'client/components/submit/submit.html',
+      controller: 'SubmitController'
+    });
+
+  $urlRouterProvider.otherwise("/");
+});
--- help2clean-angular.js
+++ help2clean-angular.js
@@ -2,30 +2,6 @@ Ads = new Mongo.Collection("ads");
 
 if (Meteor.isClient) {
 
-  // This code only runs on the client
-
-   angular.module('h2c').config(function($urlRouterProvider, $stateProvider, $locationProvider){
-   
-     $locationProvider.html5Mode(true);
- 
-     $stateProvider
-       .state('home', {
-         url: '/',
-         templateUrl: 'home.html',
-         controller: 'HomeController'
-       }).state('search', {
-         url: '/search',
-         templateUrl: 'search.html',
-         controller: 'SearchController'
-       }).state('submit', {
-         url: '/submit',
-         templateUrl: 'submit.html',
-         controller: 'SubmitController'
-       });
- 
-     $urlRouterProvider.otherwise("/");
-   });
-
    angular.module('h2c').controller('HomeController', ['$scope',
      function ($scope) {
   
