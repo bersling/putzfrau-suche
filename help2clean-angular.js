2ea73ace89295d0180e657589a8cb41357708a09
bersling <bersling@gmail.com>
2015-11-22 13:20:49 +0100

    thumbstore success!

--- help2clean-angular.js
+++ help2clean-angular.js
@@ -1,5 +1,17 @@
 Ads = new Mongo.Collection("ads");
 
+//IMAGES
+var createSquareThumb = function(fileObj, readStream, writeStream) {
+  var size = '130';
+  gm(readStream).autoOrient().resize(size, size + '^').gravity('Center').extent(size, size).stream('PNG').pipe(writeStream);
+};
+
+var thumbStore = new FS.Store.FileSystem("thumbs", { transformWrite: createSquareThumb });
+
+Images = new FS.Collection("images", { stores: [thumbStore] });
+//END IMAGES
+
+
 if (Meteor.isClient) {
 
   angular.module('h2c').controller('HomeController', ['$scope',
@@ -18,10 +30,18 @@ if (Meteor.isClient) {
 
   angular.module('h2c').controller('SubmitController', ['$scope',
     function($scope) {
-      console.log('here');
       $scope.uploadFile = function() {
-        var filename = event.target.files[0].name;
-        alert('file was selected: ' + filename);
+        FS.Utility.eachFile(event, function(file) {
+          Images.insert(file, function(err, fileObj) {
+            if (!err) {
+              // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+              Session.set('imageId', fileObj._id);
+            } else {
+              console.log(err)
+            }
+
+          });
+        });
       };
     }
   ]);
