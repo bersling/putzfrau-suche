d3253615474f616f5ab9dfbf6cb5182079fbd511
bersling <bersling@gmail.com>
2015-11-24 23:57:41 +0100

    for some reason commented out again...

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 source properties.sh
 
-#meteor build compiled
-#scp -r compiled ${server}:compiled
-#rm -rf compiled
+meteor build compiled
+scp -r compiled ${server}:compiled
+rm -rf compiled
 scp  helper.sh ${server}:helper.sh
 scp  properties.sh ${server}:properties.sh
 ssh ${server} chmod +x helper.sh
