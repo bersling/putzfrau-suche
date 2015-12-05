abc8aab5c554c2ae74acc1c520ac286fbccc5a03
bersling <bersling@gmail.com>
2015-12-05 13:38:32 +0100

    adapt resotre information

--- deployment/backup.sh
+++ deployment/backup.sh
@@ -2,5 +2,5 @@ source properties.sh
 now=$(date +%s)
 ssh ${server} mongodump --host 127.0.0.1 --port 27017 --db putzfrau-suche --out ~/dumps/${now}
 
-# to restore:
-#mongorestore --host 127.0.0.1 --port 27017 --db <dbname> --drop <path>/<to>/<dump>/<database-name>
+# to restore, go to dumped folder and run:
+#mongorestore --host 127.0.0.1 --port 27017 --db putzfrau-suche --drop ./putzfrau-suche
