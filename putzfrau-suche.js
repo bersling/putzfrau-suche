337461030336efdd4a67d06d66ecaa190a29d123
bersling <bersling@gmail.com>
2015-12-14 13:22:20 +0100

    fix mail

--- putzfrau-suche.js
+++ putzfrau-suche.js
@@ -199,7 +199,7 @@ if (Meteor.isServer) {
     sendMail: function (newAd, url) {
       var html = 'Vielen Dank, dass Sie auf putzfrau-suche.ch inseriert haben! <br> Um Ihr Inserat zu editieren, klicken Sie auf: <a href=' + url + '>' + url + '</a> <br><br> Freundliche Grüsse <br> Ihr putzfrau-suche.ch Team';
       var optionsSendInfo = {
-          from: "kontakt@putzfrau-suche.ch",
+          from: "support@putzfrau-suche.ch",
           to: [newAd.email],
           bcc: ["bersling@gmail.com"],
           subject: "Link zum Editieren auf putzfrau-suche.ch",
